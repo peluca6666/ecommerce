66dc12e81a469783f8c3dd91a30570dca0e91065
peluca6666 <gercab666@gmail.com>
2025-06-25 18:37:07 -0300

    Cambio de DB

--- backend/database/connectionMySQL.js
+++ backend/database/connectionMySQL.js
@@ -1,9 +1,11 @@
-import {createPool} from "mysql2/promise"; 
+import { createPool } from "mysql2/promise";
+import dotenv from "dotenv";
+dotenv.config();
 
-export const pool = createPool({ 
-    host: "localhost",
-    port: 3306,
-    database: "ecommerce",
-    user: "root",
-    password: "Miprimerproyecto2025!"
-})
+export const pool = createPool({
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 3306,
+  database: process.env.DB_NAME || "ecommerce",
+  user: process.env.DB_USER || "ecom_user",
+  password: process.env.DB_PASSWORD || "MiPrimerEcommerce2025@"
+});
