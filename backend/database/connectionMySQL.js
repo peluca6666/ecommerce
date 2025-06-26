f528ff772911909520b505d129b8eb50683d04d5
peluca6666 <gercab666@gmail.com>
2025-06-25 22:16:06 -0300

    subo src con app.js

--- backend/database/connectionMySQL.js
+++ backend/database/connectionMySQL.js
@@ -2,11 +2,6 @@ import { createPool } from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log('Intentando conectar con:');
-console.log('user:', process.env.DB_USER);
-console.log('password:', process.env.DB_PASSWORD);
-console.log('database:', process.env.DB_NAME);
-
 export const pool = createPool({
   host: process.env.DB_HOST || "localhost",
   port: process.env.DB_PORT || 3306,
