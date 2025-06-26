ce52c1b3dc6469ea1c0bd57fa3875fbd1039d4f5
peluca6666 <gercab666@gmail.com>
2025-06-25 21:58:58 -0300

    consolelog bd

--- backend/database/connectionMySQL.js
+++ backend/database/connectionMySQL.js
@@ -2,6 +2,11 @@ import { createPool } from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
+console.log('Intentando conectar con:');
+console.log('user:', process.env.DB_USER);
+console.log('password:', process.env.DB_PASSWORD);
+console.log('database:', process.env.DB_NAME);
+
 export const pool = createPool({
   host: process.env.DB_HOST || "localhost",
   port: process.env.DB_PORT || 3306,
@@ -9,4 +14,3 @@ export const pool = createPool({
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "MiPrimerEcommerce2025@"
 });
-console.log("USUARIO MYSQL:", process.env.DB_USER);
