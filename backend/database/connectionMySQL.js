97642f5aabbbc8134ac42d80436e24be0c098e02
peluca6666 <gercab666@gmail.com>
2025-06-25 21:57:26 -0300

    Nuevo console log BD

--- backend/database/connectionMySQL.js
+++ backend/database/connectionMySQL.js
@@ -6,6 +6,7 @@ export const pool = createPool({
   host: process.env.DB_HOST || "localhost",
   port: process.env.DB_PORT || 3306,
   database: process.env.DB_NAME || "ecommerce",
-  user: process.env.DB_USER || "ecom_user",
+  user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "MiPrimerEcommerce2025@"
 });
+console.log("USUARIO MYSQL:", process.env.DB_USER);
