18efeb22646568d3c7cc6d0c30682e010a2c27c6
peluca6666 <gercab666@gmail.com>
2025-07-24 15:04:44 -0300

    Segundo cambio de tamaño header

--- frontend/src/pages/Profile/ProfilePage.jsx
+++ frontend/src/pages/Profile/ProfilePage.jsx
@@ -55,12 +55,12 @@ const ProfilePage = () => {
     }}>
       <Row gutter={24} style={{ height: '100%' }}>
         {/* Columna izquierda - Header del perfil */}
-        <Col xs={24} lg={8} xl={6} style={{ height: '7%5' }}>
+        <Col xs={24} lg={8} xl={6} style={{ height: '85%' }}>
           <ProfileHeader profileData={profileData} />
         </Col>
 
         {/* Columna derecha - Tabs principales */}
-        <Col xs={24} lg={16} xl={18} style={{ height: '75%' }}>
+        <Col xs={24} lg={16} xl={18} style={{ height: '85%' }}>
           <Card style={{ 
             boxShadow: '0 2px 8px rgba(0,0,0,0.1)',
             height: '100%',
